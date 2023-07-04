44b800af3efa4d1b35c2dc0e3117e0ec1f85dec0
Lamartune <fatih.tun.2002@gmail.com>
2023-07-04 14:40:32 +0300

    List Group improved.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,24 @@
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List Group</h1>
+        <p>No item found!</p>;
+      </>
+    );
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List Group</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 export default ListGroup;
