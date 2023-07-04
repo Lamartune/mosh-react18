781cb0082008f3900a81452b19010bc08724809c
Lamartune <fatih.tun.2002@gmail.com>
2023-07-04 13:18:52 +0300

    Prroject created.

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing
+function Message(){
+    //JSX: JavaScript XML
+    const name = "Cabbra"
+
+    if (name)
+        return <h1>Cabbarın öfkesi hala dinmedi. {name}</h1>
+    return <h1>Hello Cabbar</h1>
+}
+
+export default Message;
