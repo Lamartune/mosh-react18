46b35973e0354afe62e6043e854580e8fca61d5b
Lamartune <fatih.tun.2002@gmail.com>
2023-07-05 11:56:01 +0300

    Added list update.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,6 +1,15 @@
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // //Event Handler
+  // const handleClick = (event: MouseEvent) => console.log(event);
 
   const getMessage = () => {
     // return items.length === 0 ? <p>No item found</p> : null;
@@ -12,8 +21,20 @@ function ListGroup() {
       <h1>List Group</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
