2c32ac4dd0d2a39fcc8c9bdff91ee3946c743ae9
Lamartune <fatih.tun.2002@gmail.com>
2023-07-24 16:55:37 +0300

    Using CSS Modules Exercise.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup";
-import "./App.css"
-import {BsFillCalendarFill} from "react-icons/bs"
+import "./App.css";
+import { BsFillCalendarFill } from "react-icons/bs";
 
 function App() {
   const [alertVisible, setAlertVisible] = useState(false);
 
   return (
     <div>
-      <BsFillCalendarFill color="blue" size="40"/>
+      <div className="mb-2">
+        <BsFillCalendarFill color="blue" size="40" />
+      </div>
       {alertVisible && (
         <Alert onClose={() => setAlertVisible(false)}>Hello Cabbar</Alert>
       )}
-      <Button color="primary" onClick={() => setAlertVisible(true)}>
-        My Button
-      </Button>
+      <Button onClick={() => setAlertVisible(true)}>My Button</Button>
       <ListGroup
         items={["New York", "Konya", "San Francisco"]}
         heading="Cities"
