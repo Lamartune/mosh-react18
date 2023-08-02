386c2b8ed4f9bf1b9488c4f76ff4ae5d5f69be8d
Lamartune <fatih.tun.2002@gmail.com>
2023-08-02 14:33:05 +0300

    Builded a Like Component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,6 +4,7 @@ import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup";
 import "./App.css";
 import { BsFillCalendarFill } from "react-icons/bs";
+import Like from "./components/Like";
 
 function App() {
   const [alertVisible, setAlertVisible] = useState(false);
@@ -13,15 +14,22 @@ function App() {
       <div className="mb-2">
         <BsFillCalendarFill color="blue" size="40" />
       </div>
+      <br></br>
       {alertVisible && (
         <Alert onClose={() => setAlertVisible(false)}>Hello Cabbar</Alert>
       )}
-      <Button onClick={() => setAlertVisible(true)}>My Button</Button>
+      <div>
+        <Button onClick={() => setAlertVisible(true)}>My Button</Button>
+      </div>
       <ListGroup
         items={["New York", "Konya", "San Francisco"]}
         heading="Cities"
         onSelecetItem={() => "asd"}
       ></ListGroup>
+      <br></br>
+      <div>
+        <Like onClick={() => console.log("clicked")} />
+      </div>
     </div>
   );
 }
